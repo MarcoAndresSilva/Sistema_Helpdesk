e87c17c43c8abbaf588851ba4cdcd965dc29e81c
Marco silva <marco.silvaponce10@gmail.com>
2024-07-23 16:52:14 -0400

    doc(libreires) agregar nuevas librerias de JS

--- view/MainHead/head.php
+++ view/MainHead/head.php
@@ -3,6 +3,12 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
 <meta http-equiv="x-ua-compatible" content="ie=edge">
 
+<link rel="stylesheet" href="../../public/css/lib/bootstrap/bootstrap.min.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+<link rel="stylesheet" href="../../public/css/main.css">
+<link rel="stylesheet" href="../../public/css/lib/font-awesome/font-awesome.min.css">
+
 <link rel="stylesheet" href="../../public/css/lib/summernote/summernote.css"/>
 <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet">
 <link rel="stylesheet" href="../../public/css/lib/summernote/summernote.css"/>
@@ -18,17 +24,13 @@
 <link rel="stylesheet" href="../../public/css/separate/vendor/datatables-net.min.css">
 
 <link rel="stylesheet" href="../../public/css/separate/vendor/select2.min.css">
+<link rel="stylesheet" href="../../public/css/separate/vendor/bootstrap-select/bootstrap-select.min.css">
 
 <link rel="stylesheet" href="../../public/css/main.css">
 
 <link rel="stylesheet" href="../../public/css/separate/vendor/fancybox.min.css">
 <link rel="stylesheet" href="../../public/css/separate/pages/activity.min.css">
 
-
 <link rel="stylesheet" href="../../public/css/separate/pages/editor.min.css">
 <link rel="stylesheet" href="../../public/css/separate/pages/login.min.css">
 
-<link rel="stylesheet" href="../../public/css/lib/font-awesome/font-awesome.min.css">
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-<!-- <link rel="stylesheet" href="../../public/css/lib/bootstrap/bootstrap.min.css"> -->
-<link rel="stylesheet" href="../../public/css/main.css">
