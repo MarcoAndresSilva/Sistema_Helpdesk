e8307a5245e4d51d6c79b103f9ad3cfda8bd9511
Marco silva <marco.silvaponce10@gmail.com>
2024-07-23 16:58:21 -0400

    refactor(index) se cambio el diseño del modal

--- view/MantenedorUsuarios/modalMantenimiento.php
+++ view/MantenedorUsuarios/modalMantenimiento.php
@@ -1,44 +1,46 @@
 
-
-<div id="modalmantenimiento" class="modal fade bd-example-modal-lg"
-					 tabindex="-1"
-					 role="dialog"
-					 aria-labelledby="myLargeModalLabel"
-					 aria-hidden="true">
-					<div class="modal-dialog modal-lg">
-						<div class="modal-content">
-							<div class="modal-header">
-								<button type="button" class="modal-close" data-dismiss="modal" aria-label="Close">
-									<i class="font-icon-close-2"></i>
-								</button>
-								<h4 class="modal-title" id="mldtitle"></h4>
-							</div>
-							<div class="modal-body">
-								...
-							</div>
-							<div class="modal-footer">
-								<button type="button" class="btn btn-rounded btn-default" data-dismiss="modal">Cerrar</button>
-				<button type="button" class="btn btn-rounded btn-primary">Guardar</button>
-							</div>
-						</div>
-					</div>
-				</div><!--.modal-->
-
 <!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div id="modalmantenimiento" class="modal fade bd-example-modal-lg" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+				<button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
       </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
+			<h4 id="mdlTitle">Mantenedor de usuarios</h4>
+
+      <form method="post" id="usuario_form">
+        <div class="modal-body">
+            <input type="hidden" id="user_id" name="user_id">
+            <fieldset class="form-group">
+              <label class="form-label semibold" for="user_name">Nombre</label>
+              <input type="text" class="form-control" id="user_name" placeholder="Nombre" required>
+            </fieldset>
+            <fieldset class="form-group">
+              <label class="form-label semibold" for="user_lastname">Apellido</label>
+              <input type="text" class="form-control" id="user_lastname" placeholder="Apellido" required>
+            </fieldset>
+            <fieldset class="form-group">
+              <label class="form-label" for="user_mail">Email</label>
+              <input type="email" class="form-control" id="user_mail" placeholder="ingrese email" required>
+            </fieldset>
+            <fieldset class="form-group">
+              <label class="form-label" for="rol_id">Rol</label>
+              <select class="bootstrap-select" id="rol_id">
+                    <option value="1">Usuario</option>
+                    <option value="2">Soporte</optiond=>
+              </select>
+            </fieldset>
+            <fieldset class="form-group">
+              <label class="form-label" for="user_mail">Contraseña</label>
+              <input type="text" class="form-control" id="user_pass" placeholder="*************" required>
+            </fieldset>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary btn-default" data-dismiss="modal">Cerrar</button>
+          <button type="submit" name="action" id="#" value="add"  class="btn btn-primary">Guardar</button>
+        </div>
+      </form>
+
     </div>
   </div>
 </div>
