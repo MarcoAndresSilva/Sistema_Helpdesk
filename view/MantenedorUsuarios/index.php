bc90d2e382b57ef257b37dd1f73b463ebbe1e069
Marco silva <marco.silvaponce10@gmail.com>
2024-07-23 16:54:58 -0400

    refactro(index) refactorizar el archivo html

--- view/MantenedorUsuarios/index.php
+++ view/MantenedorUsuarios/index.php
@@ -6,6 +6,7 @@ if (isset($_SESSION["user_id"])) {
 <!DOCTYPE html>
 <html>
 	<?php require_once("../MainHead/head.php");?>
+
 	<title>Sistema Helpdesk - Mantenedor Usuarios</title>
 </head>
 
@@ -19,56 +20,47 @@ if (isset($_SESSION["user_id"])) {
 	<?php require_once("../MainNav/nav.php");?>
 
 	<!-- contenido  -->
-	<div class="page-content">
-		<div class="container-fluid">
+	 <!-- contenido  -->
+    <div class="page-content">
+      <div class="container-fluid">
+        <header class="section-header">
+          <div class="tbl">
+            <div class="tbl-row">
+              <div class="tbl-cell">
+                <h3>Mantenedor Usuarios</h3>
+                <ol class="breadcrumb breadcrumb-simple">
+                  <li><a href="#">Home</a></li>
+                  <li class="active">Mantenedor Usuarios</li>
+                </ol>
+              </div>
+            </div>
+          </div>
+        </header>
 
-				<header class="section-header">
-					<div class="tbl">
-						<div class="tbl-row">
-							<div class="tbl-cell">
-								<h3>Mantenedor Usuarios</h3>
-								<ol class="breadcrumb breadcrumb-simple">
-									<li><a href="#">Home</a></li>
-									<li class="active">Mantenedor Usuarios</li>
+        <div class="box-typical box-typical-padding">
+          <button type="button" id="btnNuevoRegistro" class="btn btn-inline btn-primary" data-bs-toggle="modal" data-bs-target="#modalmantenimiento">
+            Nuevo Usuario +
+          </button>
 
-								</ol>
-							</div>
-						</div>
-					</div>
-			</header>
+          <table id="usuario_data" class="table table-bordered table-striped table-vcenter js-dataTable-full">
+            <thead>
+              <tr>
+                <th style="width:5%;">Nombre</th>
+                <th style="width:10%;">Apellido</th>
+                <th class="d-none d-sm-table-cell" style="width:20%;">Correo</th>
+                <th class="d-none d-sm-table-cell" style="width:10%;">contraseña</th>
+                <th class="d-none d-sm-table-cell" style="width:15%;">Rol</th>
+                <th class="text-center" style="width:5%;">Editar</th>
+                <th class="text-center" style="width:5%;">Eliminar</th>
+              </tr>
+            </thead>
+            <tbody></tbody>
+          </table>
+        </div>
+      </div>
+    </div>
 
-			<div class="box-typical box-typical-padding">
-				<button type="button" id="btnNuevoRegistro" class="btn btn-inline btn-primary-outline data-bs-toggle="modal" data-bs-target="#.bd-example-modal-lg">Nuevo Registro +</button>
-								<!-- Button trigger modal -->
-			<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-				Launch demo modal
-			</button>
-
-				<table id="usuario_data" class="table table-bordered table-striped table-vcenter js-dataTable-full">
-					<thead>
-						<tr>
-							<th style="width:5%;">Nombre</th>
-							<th style="width:10%;">Apellido</th>
-							<th class="d-none d-sm-table-cell" style="width:20%;">Correo</th>
-							<th class="d-none d-sm-table-cell" style="width:10%;">contraseña</th>
-							<th class="d-none d-sm-table-cell" style="width:15%;">Rol</th>
-							<th class="text-center" style="width:5%;">Editar</th>
-							<th class="text-center" style="width:5%;">Eliminar</th>
-						</tr>
-					</thead>
-					<tbody>
-
-					</tbody>
-				</table>
-			</div>
-
-		</div>
-	</div>
-
-	<!--.page-content-->
-
-
-  <?php require_once("modalMantenimiento.php");?>
+  <?php require_once("modalmantenimiento.php");?>
 	<?php require_once("../MainJs/js.php");?>
 	<script type="text/javascript" src="mantenedorusuarios.js"></script>
 
@@ -80,4 +72,5 @@ if (isset($_SESSION["user_id"])) {
 }else{
 	header("Location:".Conectar::ruta()."index.php");
 }
+
 ?>
