8e9ce4911fd6384155f3bf01b2b885b29c9c6eb6
Marco silva <marco.silvaponce10@gmail.com>
2024-07-23 16:54:21 -0400

    doc(libreries) agregar nuevas librerias

--- view/MainJs/js.php
+++ view/MainJs/js.php
@@ -10,11 +10,15 @@
 	<script src="../../public/js/lib/datatables-net/datatables.min.js"></script>
 	<script src="../../public/js/lib/fancybox/jquery.fancybox.pack.js"></script>
 
+	<script src="../../public/js/lib/jquery-tag-editor/jquery.caret.min.js"></script>
+	<script src="../../public/js/lib/jquery-tag-editor/jquery.tag-editor.min.js"></script>
+	<script src="../../public/js/lib/bootstrap-select/bootstrap-select.min.js"></script>
+	<script src="../../public/js/lib/select2/select2.full.min.js"></script>
 
-	<script type="text/javascript" src="../../public/js/lib/jqueryui/jquery-ui.min.js"></script>
-	<script type="text/javascript" src="../../public/js/lib/lobipanel/lobipanel.min.js"></script>
-	<script type="text/javascript" src="../../public/js/lib/match-height/jquery.matchHeight.min.js"></script>
-	<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+	<script src="../../public/js/lib/jqueryui/jquery-ui.min.js"></script>
+	<script src="../../public/js/lib/lobipanel/lobipanel.min.js"></script>
+	<script src="../../public/js/lib/match-height/jquery.matchHeight.min.js"></script>
+	<script src="https://www.gstatic.com/charts/loader.js"></script>
 
 	<script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
 	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAdCMoRAl_-ARUflpa4Jn_qUoOpdXlxQEg&libraries=places"></script>
