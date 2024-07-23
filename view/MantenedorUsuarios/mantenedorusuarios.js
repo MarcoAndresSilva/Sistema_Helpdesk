777b8d1d5707c92137722c2d9481980a4edc2c73
Marco silva <marco.silvaponce10@gmail.com>
2024-07-23 16:55:51 -0400

    feat(js) crear funciones necesarias para guardar editar

--- view/MantenedorUsuarios/mantenedorusuarios.js
+++ view/MantenedorUsuarios/mantenedorusuarios.js
@@ -1,5 +1,24 @@
 let tabla;
-function init() {}
+function init() {
+  $("usuario_form").on("submit", function (e) {
+    guardaryeditar(e);
+  });
+}
+
+function guardaryeditar() {
+  e.preventDefault();
+  var formData = new FormData($("#usuario_form")[0]);
+  $.ajax({
+    url: "../../controller/usuario.php?op=guardaryeditar",
+    tyoe: "POST",
+    data: formData,
+    contentType: false,
+    processData: false,
+    success: function (datos) {
+      console.log(datos);
+    },
+  });
+}
 
 $(document).ready(function () {
   // console.log("asdgda");
@@ -54,25 +73,46 @@ $(document).ready(function () {
       },
     })
     .DataTable();
-
   ///////////////////////////////////////
 
-  //  $.post(
-  //    "../../controller/usuario.php?op=listar",
-  //    function (respuesta, status) {
-  //      // Parsear la respuesta JSON
-  //      console.log(respuesta);
-  //      var data = JSON.parse(respuesta);
-  //      console.log(data);
+  $.post(
+    "../../controller/usuario.php?op=listar",
+    function (respuesta, status) {
+      // Parsear la respuesta JSON
+      console.log(respuesta);
+      var data = JSON.parse(respuesta);
+      console.log(data);
 
-  //     //  $("#usuario_data").html(data.critico);
-  //    }
-  //  );
+      $("#usuario_data").html(data.critico);
+    }
+  );
 });
 
 function editar(user_id) {
-  $("#modalTitulo").html("Editar Registro");
-  console.log(user_id);
+  $("#mdlTitle").html("Editar Registro");
+  $.post(
+    "../../controller/usuario.php?op=mostrar",
+    { user_id: user_id },
+    function (data) {
+      try {
+        // datos recibidos en formato JSON
+        var data = JSON.parse(data);
+        console.log(data);
+
+        // Aquí cargar los datos en los campos del modal
+        $("#user_id").val(data.user_id);
+        $("#user_name").val(data.user_name);
+        $("#user_lastname").val(data.user_lastname);
+        $("#user_mail").val(data.user_mail);
+        $("#user_pass").val(data.user_pass);
+        $("#rol_id").val(data.rol_id).trigger("change");
+      } catch (e) {
+        console.error("Error parsing JSON:", e);
+        console.log("Received data:", data);
+      }
+    }
+  );
+  $("#modalmantenimiento").modal("show");
 }
 
 function eliminar(user_id) {
@@ -110,10 +150,11 @@ function eliminar(user_id) {
   );
 }
 
-$(document).on("click", "#btnNuevoRegistro", function () {
-  // console.log("nueoRegistro");
+// Mostrar modal al hacer clic en el botón "Nuevo Registro"
+$("#btnNuevoRegistro").on("click", function () {
+  $("#mdlTitle").text("Datos Nuevo Registro");
+  $("#usuario_form")[0].reset();
   $("#modalmantenimiento").modal("show");
-  // $("#modalTitulo").html("Nuevo Registro");
 });
 
 init();
